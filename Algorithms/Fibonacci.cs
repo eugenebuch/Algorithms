c5de8716db6b7cb538314506640753b6fee42aff
Евгений Евгеньевич Бученков <eugenebuch@niuitmo.ru>
2020-09-05 22:43:58 +0300

    Fibonacci edited

--- Algorithms/Fibonacci.cs
+++ Algorithms/Fibonacci.cs
@@ -14,13 +14,20 @@ namespace Algorithms
             }
             else
             {
-                int prev = 0, curr = 1;
+                long prev = 0, curr = 1;
                 while (n > 0)
                 {
-                    Console.WriteLine(curr);
-                    curr += prev;
-                    prev = curr - prev;
-                    n--;
+                    try
+                    {
+                        Console.Write(curr + (n == 1 ? "" : ", "));
+                        curr = checked(curr + prev);
+                        prev = curr - prev;
+                        n--;
+                    } catch
+                    {
+                        Console.WriteLine("The num is too big, can't continue working...");
+                        break;
+                    }
                 }
             }
         }
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -6,7 +6,7 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            Fibonacci.Draw(30);
+            Fibonacci.Draw(30000);
             // Huffman.Handling.Encode("abracadabra");
             // Console.WriteLine(SumWithoutOperators.Sum(7, 5));
         }
